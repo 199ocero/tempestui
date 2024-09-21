0497a7d9354cca6f87453f35f31524251c0b7167
199ocero <199ocero@gmail.com>
2024-09-22 07:23:01 +0800

    added card general component

---
+++ src/Components/Layouts/Card.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace JaOcero\TempestUi\Components\Layouts;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use JaOcero\TempestUi\Services\CardCssBuilder;
+
+class Card extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('tempestui::components.layouts.card', [
+            'parentCss' => CardCssBuilder::parentCss(),
+        ]);
+    }
+}
