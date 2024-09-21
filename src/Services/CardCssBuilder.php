67bf8ff6a64247bb6b375b3be37d004b65bc08f6
199ocero <199ocero@gmail.com>
2024-09-22 07:23:10 +0800

    added card general component

---
+++ src/Services/CardCssBuilder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JaOcero\TempestUi\Services;
+
+class CardCssBuilder
+{
+    public static function parentCss(): string
+    {
+        return 'w-full p-5 rounded-lg bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-800';
+    }
+}
